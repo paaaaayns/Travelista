a8bec18a78b4cb2a99d868d8aa7f62866436254c
Kuzu <ejpines21@gmail.com>
2023-04-11 00:29:55 +0800

    Initial Commit

--- app/src/main/java/com/example/travelista/DirectionsPage.java
+++ app/src/main/java/com/example/travelista/DirectionsPage.java
@@ -2,13 +2,51 @@ package com.example.travelista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class DirectionsPage extends AppCompatActivity {
+
+    ImageView btnBack;
+
+    EditText edtOrigin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directions_page);
+
+        edtOrigin = findViewById(R.id.edtOrigin);
+
+        edtOrigin.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_RIGHT = 2;
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (edtOrigin.getRight() - edtOrigin.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        edtOrigin.setText("");
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        });
+
+
+                btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/travelista/MapPage.java
+++ app/src/main/java/com/example/travelista/MapPage.java
@@ -1,11 +1,10 @@
 package com.example.travelista;
 
+import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -18,16 +17,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import android.Manifest;
 
-import com.google.android.gms.internal.location.zzau;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -39,30 +36,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.karumi.dexter.Dexter;
-import com.karumi.dexter.PermissionToken;
-import com.karumi.dexter.listener.PermissionDeniedResponse;
-import com.karumi.dexter.listener.PermissionGrantedResponse;
-import com.karumi.dexter.listener.PermissionRequest;
-import com.karumi.dexter.listener.single.PermissionListener;
-
 public class MapPage extends AppCompatActivity implements OnMapReadyCallback {
 
     DrawerLayout drawerLayout;
-    ImageView menu;
+    ImageView btnMenu;
     LinearLayout layoutHome, layoutDirections, layoutGoToPlaces, layoutItinerary, layoutAbout, layoutSignOut;
 
     TextView lblName, lblEmail;
 
 
-
     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://useful-maxim-382603-default-rtdb.firebaseio.com/");
 
-
-    SupportMapFragment mapFragment;
-    FusedLocationProviderClient locationProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,7 +54,7 @@ public class MapPage extends AppCompatActivity implements OnMapReadyCallback {
         setContentView(R.layout.activity_map_page);
 
         drawerLayout = findViewById(R.id.drawerLayout);
-        menu = findViewById(R.id.menu);
+        btnMenu = findViewById(R.id.btnMenu);
         layoutHome = findViewById(R.id.layoutHome);
         layoutDirections = findViewById(R.id.layoutDirections);
         layoutGoToPlaces = findViewById(R.id.layoutGoToPlaces);
@@ -102,7 +86,7 @@ public class MapPage extends AppCompatActivity implements OnMapReadyCallback {
         });
 
         // Open Navigation Drawer
-        menu.setOnClickListener(new View.OnClickListener() {
+        btnMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openDrawer(drawerLayout);
@@ -136,31 +120,22 @@ public class MapPage extends AppCompatActivity implements OnMapReadyCallback {
         });
 
 
-        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.Map);
-        mapFragment.getMapAsync(this);
+        // Get Location
+        locationProvider = LocationServices.getFusedLocationProviderClient(this);
 
-        locationProvider = (FusedLocationProviderClient) LocationServices.getFusedLocationProviderClient(this);
-        Dexter.withContext(getApplicationContext()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
-            @Override
-            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                getCurrentLocation();
-            }
+        getLocation();
 
-            @Override
-            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
 
-            }
 
-            @Override
-            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-                permissionToken.continuePermissionRequest();
-            }
-        }).check();
     }
 
-    public void getCurrentLocation() {
+    final int FINE_PERMISSION_CODE = 1;
+    Location currentLocation;
+    SupportMapFragment mapFragment;
+    FusedLocationProviderClient locationProvider;
 
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -168,51 +143,51 @@ public class MapPage extends AppCompatActivity implements OnMapReadyCallback {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
             return;
         }
+
+
         Task<Location> task = locationProvider.getLastLocation();
 
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
-                mapFragment.getMapAsync(new OnMapReadyCallback() {
-                    @Override
-                    public void onMapReady(@NonNull GoogleMap googleMap) {
-                        if (location !=null) {
-                            LatLng currentLoc = new LatLng(location.getLatitude(),location.getLongitude());
-                            MarkerOptions marker = new MarkerOptions().position(currentLoc).title("You are here!").visible(true);
-                            googleMap.addMarker(marker);
-                            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLoc, 15f));
-                        }
+                if (location != null) {
+                    currentLocation = location;
 
-                        else {
-                            Toast.makeText(MapPage.this, "Please turn on your Location App Permission.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+                    mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.Map);
+                    mapFragment.getMapAsync(MapPage.this);
+                }
             }
         });
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        if (requestCode == FINE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLocation();
+            } else {
+                Toast.makeText(this, "Please turn on Location Permissions.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 
+    GoogleMap gMaps;
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMaps = googleMap;
 
-
-
-
-
-
-
-
-
-
-
-
+        LatLng myLocation = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        gMaps.addMarker(new MarkerOptions().position(myLocation).title("You are here!"));
+        gMaps.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 15f));
+    }
 
 
     // OPEN NAVIGATION DRAWER
@@ -232,7 +207,6 @@ public class MapPage extends AppCompatActivity implements OnMapReadyCallback {
         Intent intent = new Intent(activity, secondActivity);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         activity.startActivity(intent);
-        activity.finish();
     }
 
     @Override
